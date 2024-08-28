d4527c9e2e949e51b404e0a8c8fb6634fbcb8711
nagobaa <fej@hotmail.fi>
2024-08-28 09:06:21 +0300

    tillsatt av filer

---
+++ uppgifter/uppgift_1.java
@@ -0,0 +1,20 @@
+public class newJavaFile{
+
+    public static void main(String args []) {
+        
+        String FirstName = "Mats";
+        String LastName = "Petterson";
+        int age = 20;      
+        double height = 1.80;
+        boolean IsAStudent = true;
+        
+        System.out.print("The first name is " + FirstName + " and ");
+        System.out.println("the last name is " + LastName+ ".");
+        System.out.print("The age is " + age + "");
+        System.out.println("and the height is " + height + "cm.");
+        System.out.print("Is he a student? ");
+        if (IsAStudent = true){
+            System.out.println("Yes, he is a student.");
+        }
+    }
+}
