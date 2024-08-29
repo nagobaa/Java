8306d25fc3aa4632be5e07a736d258d9c0f1cc93
nagobaa <fej@hotmail.fi>
2024-08-29 12:55:49 +0300

    Uppgiften är gjord klar

--- uppgifter/uppgift_2.java
+++ uppgifter/uppgift_2.java
@@ -1,6 +1,21 @@
-public class newJavaFile {
-    public static void main(String args [] {
+public class newJavaFile{
+    
+    public static void main(String args[]){
+        //string, int, float, char, bool
         
+        //string
+        String name = "Bob";
+        System.out.println(name);
         
-    }
+        //int (heltal, positiva (?))
+        int Num = 10;
+        System.out.println(Num);
+        
+        //Float
+        float FloatNum = 5.99f;
+        System.out.println(FloatNum);
+        //Char (Character)
+        
+        char char1 = 'H';
+    }        
 }
