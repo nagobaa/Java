bd9551ddfaa679f6656b5ec30d371a9a77ad2b70
nagobaa <fej@hotmail.fi>
2024-08-29 13:58:08 +0300

    uppdateringar

---
+++ uppgifter/uppgift_3.java
@@ -0,0 +1,31 @@
+public class NewJavaFile {
+    
+    public static void main(String args[]) {
+        //deklaration av variabler
+        int number1 = 1; 
+        float number2 = 2.5f; 
+        double number3 = 2.55;
+        char number4 = 'A';
+        int number5 = 2;
+        
+        //utskrift av de nya värden
+        System.out.println("The integer for the variable named number 1 is " + (double) number1);
+        System.out.println("The float value for the variable named number 2 is " + (double) number2);
+        System.out.println("The double value for the variable named number 3 är " + (int) number3);
+        System.out.println("The \"char"\ for the variable number 4 is " + number4);
+        System.out.println("The integer för the varible named number 5 is " + number5);
+        
+        //utskrift av de nya värden
+        //int number1 → double
+        System.out.println("The integer for the variable named number 1 is " + (double) number1);
+        //float number2 → doubble
+        System.out.println("The float value for the variable named number 2 is " + (double) number2);
+        //double nummber3 → int
+        System.out.println("The double value for the variable named number 3 är " + (int) number3);
+        //char nummber4 → int
+        System.out.println("The "\char\" for the variable number 4 is " + (int) number4);
+        //int number5 → byte
+        System.out.println("The integer för the varible named number 5 is " + (byte) number5);
+    }
+}    
+        
