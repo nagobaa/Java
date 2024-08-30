04a3262ab38c79c0e905c23604347bd6b83eb704
nagobaa <fej@hotmail.fi>
2024-08-30 09:04:47 +0300

    Uppdateringar

--- uppgifter/uppgift_3.java
+++ uppgifter/uppgift_3.java
@@ -11,21 +11,23 @@ public class NewJavaFile {
         //utskrift av de nya värden
         System.out.println("The integer for the variable named number 1 is " + (double) number1);
         System.out.println("The float value for the variable named number 2 is " + (double) number2);
-        System.out.println("The double value for the variable named number 3 är " + (int) number3);
-        System.out.println("The \"char"\ for the variable number 4 is " + number4);
-        System.out.println("The integer för the varible named number 5 is " + number5);
+        System.out.println("The double value for the variable named number 3 is " + (int) number3);
+        System.out.println("The char for the variable number 4 is " + number4);
+        System.out.println("The integer for the varible named number 5 is " + number5);
         
+        //en tom rad
+        System.out.println();
         //utskrift av de nya värden
         //int number1 → double
-        System.out.println("The integer for the variable named number 1 is " + (double) number1);
+        System.out.println("The double for the originally integer variable named number 1 is " + (double) number1);
         //float number2 → doubble
-        System.out.println("The float value for the variable named number 2 is " + (double) number2);
+        System.out.println("The float value for the originally float variable named number 2 is " + (double) number2);
         //double nummber3 → int
-        System.out.println("The double value for the variable named number 3 är " + (int) number3);
+        System.out.println("The double value for the originally integer variable named number 3 is " + (int) number3);
         //char nummber4 → int
-        System.out.println("The "\char\" for the variable number 4 is " + (int) number4);
+        System.out.println("The char for the originally integer variable number 4 is " + (int) number4);
         //int number5 → byte
-        System.out.println("The integer för the varible named number 5 is " + (byte) number5);
+        System.out.println("The integer for the originally byte varible named number 5 is " + (byte) number5);
     }
 }    
         
