a2693cdf0af7dcefd5d850507fe3889de26f0b22
nagobaa <fej@hotmail.fi>
2024-08-30 14:17:04 +0300

    uppdateringar

--- uppgifter/uppgift_3.java
+++ uppgifter/uppgift_3.java
@@ -1,18 +1,3 @@
-/*
-Mål:  Deklarera och initiera variabler och utför data ändring på dem. Printa ut resultaten.
-
-Uppgiftsbeskrivning:  Skriv ett Java program var du Deklarerar variabler av olika datatyper (int, double, float, char, byte).
-
-Ändra data typen från: 
-
-    Int → double
-    float → doubble
-    double → int
-    char → int
-    int → byte
-
-Skriver ut originalvärdena och värdena efter data ändrigen i ett strukturerat format.
-*/
 public class NewJavaFile {
         
         public static void main(String args[]){
@@ -25,10 +10,25 @@ public class NewJavaFile {
         int variable5 = 4;
 
         //utskrift
-        System.out.println(variable1);
-        System.out.println(variable2);
-        System.out.println(variable3);
-        System.out.println(variable4);
-        System.out.println(variable5);
+        System.out.println("This is originally in int: " + variable1 + ".");
+        System.out.println("This is originally in float: " + variable2 + ".");
+        System.out.println("This is originally in double: " + variable3 + ".");
+        System.out.println("This is originally in char: " + variable4 + ".");
+        System.out.println("This is originally in int: " + variable5 + ".");
+        
+        //tom rad
+        System.out.println(""); //detta gör en tom rad
+        
+        //utskrift mmed nya 
+        System.out.println("This should now be printed in double: " + ((double) variable1) + ".");
+        System.out.println("This should now be printed in double: " + ((double) variable2) + ".");
+        System.out.println("This should now be printed in int: " + ((int) variable3) + ".");
+        System.out.println("This should now be printed in int: " + ((int) variable4) + ".");
+        System.out.println("This should now be printed in byte: " + ((byte) variable5) + ".");
     }
 }
+
+
+
+((int/float/double/byte etc) VariabelnamnetKommerHit)
+
