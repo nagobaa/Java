2b9e7fa1ddf40f1e3cab83da4eed5b7085af3f03
nagobaa <fej@hotmail.fi>
2024-09-03 13:38:38 +0300

    uppdateringar

--- uppgifter/uppgift_8.java
+++ uppgifter/uppgift_8.java
@@ -1,18 +1,32 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
+Ändra slingan så att den slutar skriva när siffran 11 nås. 
+Använd "break"-satsen för att sluta slingan när villkoret är uppfyllt.
+När slignan är slutad, skriv ut "Loop terminated early due to break statement."
 
-/**
- *
- * @author Frank
- */
-public class uppgift_8 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
-    }
+*/
+public class NewJavaFile {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 1; i < 31; i++){
+            if (i <30){
+            System.out.print(i + ", ");
+        } else{
+            System.out.println(i);
+            }
+        }
+        
+        for (int i = 1; i < 31; i++){
+            if (i == 11){
+                System.out.print(i + " ");
+                System.out.println("Loop terminated early due to break statement.");
+                    break;
+            } if (i <30){
+            System.out.print(i + ", ");
+            } else{
+                System.out.print(i);
+            }
+        }        
+    } 
 }
+
