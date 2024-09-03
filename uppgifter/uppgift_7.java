6e019621327c5de91fc0ee7dfd425fd65a81fd30
nagobaa <fej@hotmail.fi>
2024-09-03 13:13:05 +0300

    uppdateringar

--- uppgifter/uppgift_7.java
+++ uppgifter/uppgift_7.java
@@ -1,9 +1,8 @@
 /*
-utskrift av den första och sista siffran i tabellen
-slinga för att få summan av tabellen (taulukko, array) + division med 2 + utskrift av svaret
 BONUS: slinga för att hitta det högsta värdet i tabellen (taulukko, array)
 */
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class NewJavaFile {
     
@@ -21,14 +20,37 @@ public class NewJavaFile {
         //detta är en "endimensionell" tabell
         System.out.println(Arrays.toString(array1));
         
-        
         //utskrift av det första och sista värdet av tabellen (taulukko, array)
         System.out.println("The first number in the array1 is " + array1 [0]);
         System.out.println("The second number in the array1 is " + array1 [4]);
+        
+        //summan av tabellen (taulukko, array)
+        int sum = 0;
+        for (int i : array1) {
+            sum += i;
+        }
+        System.out.println("The sum of array1 is " + sum);
+        System.out.println("The sum of array1 divided by 2 is " + (sum / 2));
+        
+        //det maximala värdet i tabellen (taulukko, array)
+        int max = array1 [0];
+        
+        for(int i = 0; i < array1.length; i++){
+            if(max < array1 [i]){
+                max = array1 [i];
+            }
+        }
+        System.out.println("The maximal value of the array is " + max);
     }
 }
 
 
 
+
+
+ 
+        
+        
+        
 
 
